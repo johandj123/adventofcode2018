65f767a659970b618bb86adc2a55381ea2f44b16
Johan de Jong <johan.de.jong@sogeti.com>
2024-03-29 13:41:10 +0100

    Day 12 part 2.

--- src/Day12.java
+++ src/Day12.java
@@ -1,4 +1,5 @@
 import lib.InputUtil;
+import lib.PredictUtil;
 
 import java.io.IOException;
 import java.util.*;
@@ -6,6 +7,25 @@ import java.util.*;
 public class Day12 {
     public static void main(String[] args) throws IOException {
         List<String> input = InputUtil.readAsStringGroups("input12.txt");
+        first(input);
+        second(input);
+    }
+
+    private static void first(List<String> input) {
+        List<Long> sums = calculateSums(input, 20);
+        System.out.println(sums.get(sums.size() - 1));
+    }
+
+    private static void second(List<String> input) {
+        List<Long> sums = calculateSums(input, 1000000);
+        int tail = 2000;
+        int start = sums.size() - tail;
+        List<Long> sumsTail = sums.subList(start, sums.size());
+        PredictUtil.Predictor predictor = PredictUtil.predict(sumsTail);
+        System.out.println(predictor.get(50000000000L - start - 1));
+    }
+
+    private static List<Long> calculateSums(List<String> input, int count) {
         String initialString = input.get(0).substring(15);
         Set<Integer> initialState = new HashSet<>();
         for (int i = 0; i < initialString.length(); i++) {
@@ -19,11 +39,13 @@ public class Day12 {
             rules.put(sp[0], sp[1].charAt(0) == '#');
         }
         Set<Integer> state = new HashSet<>(initialState);
-        for (int i = 0; i < 20; i++) {
+        List<Long> sums = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
             state = evolve(state, rules);
+            long sum = state.stream().mapToInt(x -> x).sum();
+            sums.add(sum);
         }
-        long sum = state.stream().mapToInt(x -> x).sum();
-        System.out.println(sum);
+        return sums;
     }
 
     private static String stateAsString(Set<Integer> state,int position) {
