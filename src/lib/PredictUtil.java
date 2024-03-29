bbe6898c60efafb060d0ed5547b8d7421a378565
Johan de Jong <johan.de.jong@sogeti.com>
2024-03-29 11:31:56 +0100

    Prediction util for alternating sequences.

--- src/lib/PredictUtil.java
+++ src/lib/PredictUtil.java
@@ -16,6 +16,32 @@ public class PredictUtil {
         return null;
     }
 
+    public static AlternatingPredictor predictAlternating(List<Long> values) {
+        for (int period = 1; period <= values.size() / 4; period++) {
+            AlternatingPredictor predictor = predictAlternating(values, period);
+            if (predictor != null) {
+                return predictor;
+            }
+        }
+        return null;
+    }
+
+    public static AlternatingPredictor predictAlternating(List<Long> values, int period) {
+        List<Predictor> predictors = new ArrayList<>();
+        for (int i = 0; i < period; i++) {
+            List<Long> subValues = new ArrayList<>();
+            for (int j = i; j < values.size(); j += period) {
+                subValues.add(values.get(j));
+            }
+            Predictor predictor = predict(subValues);
+            if (predictor == null) {
+                return null;
+            }
+            predictors.add(predictor);
+        }
+        return new AlternatingPredictor(predictors);
+    }
+
     private static List<Long> calculateDeltas(List<Long> values) {
         List<Long> result = new ArrayList<>();
         for (int i = 0; i < values.size() - 1; i++) {
@@ -33,9 +59,9 @@ public class PredictUtil {
     }
 
     public static class Predictor {
-        final long x0;
-        final long x1;
-        final long x2;
+        private final long x0;
+        private final long x1;
+        private final long x2;
 
         public Predictor(long x0, long x1, long x2) {
             this.x0 = x0;
@@ -45,6 +71,19 @@ public class PredictUtil {
 
         public long get(long index) {
             return x0 + index * x1 + x2 * (((index - 1) * (index - 1) + index) / 2);
+        }
+    }
+
+    public static class AlternatingPredictor {
+        private final List<Predictor> predictors;
+
+        private AlternatingPredictor(List<Predictor> predictors) {
+            this.predictors = predictors;
+        }
+
+        public long get(long index) {
+            int predictorIndex = (int) (index % predictors.size());
+            return predictors.get(predictorIndex).get(index / predictors.size());
         }
     }
 }
