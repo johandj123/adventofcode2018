877eadcff65c5d19e350ecac09fa6760d5e281f1
Johan de Jong <johan.de.jong@sogeti.com>
2024-03-31 11:21:19 +0200

    Day 15 part 2.

--- src/Day15.java
+++ src/Day15.java
@@ -7,6 +7,27 @@ import java.util.stream.Collectors;
 
 public class Day15 {
     public static void main(String[] args) throws IOException {
+        first();
+        second();
+    }
+
+    private static void first() throws IOException {
+        Result result = simulate(3);
+        System.out.println(result.rounds * result.hitPointsLeft);
+    }
+
+    private static void second() throws IOException {
+        for (int i = 4; ; i++) {
+            System.out.println(":" + i);
+            Result result = simulate(i);
+            if (!result.elfDied) {
+                System.out.println(result.rounds * result.hitPointsLeft);
+                break;
+            }
+        }
+    }
+
+    private static Result simulate(int elfAttackPower) throws IOException {
         CharMatrix charMatrix = new CharMatrix(InputUtil.readAsLines("input15.txt"));
         List<Unit> units = new ArrayList<>();
         for (int y = 0; y < charMatrix.getHeight(); y++) {
@@ -14,13 +35,14 @@ public class Day15 {
                 CharMatrix.Position position = charMatrix.new Position(x, y);
                 char c = position.get();
                 if (c == 'E') {
-                    units.add(new Unit(UnitType.ELF, position));
+                    units.add(new Unit(UnitType.ELF, elfAttackPower, position));
                 } else if (c == 'G') {
-                    units.add(new Unit(UnitType.GOBLIN, position));
+                    units.add(new Unit(UnitType.GOBLIN, 3, position));
                 }
             }
         }
         int rounds = 0;
+        boolean elfDied = false;
         outer:
         while (true) {
             units.sort(null);
@@ -48,13 +70,16 @@ public class Day15 {
                     unit.attack(enemy);
                     if (enemy.isDead()) {
                         enemy.position.set('.');
+                        if (enemy.unitType == UnitType.ELF) {
+                            elfDied = true;
+                        }
                     }
                 }
             }
             rounds++;
         }
-        int hitpointsLeft = units.stream().filter(Unit::isAlive).mapToInt(u -> u.hitPoints).sum();
-        System.out.println(rounds * hitpointsLeft);
+        int hitPointsLeft = units.stream().filter(Unit::isAlive).mapToInt(u -> u.hitPoints).sum();
+        return new Result(rounds, hitPointsLeft, elfDied);
     }
 
     static private CharMatrix.Position determineMovePosition(Unit unit, List<Unit> enemies) {
@@ -93,12 +118,13 @@ public class Day15 {
 
     static class Unit implements Comparable<Unit> {
         final UnitType unitType;
+        final int attackPower;
         CharMatrix.Position position;
-        int attackPower = 3;
         int hitPoints = 200;
 
-        public Unit(UnitType unitType, CharMatrix.Position position) {
+        public Unit(UnitType unitType, int attackPower, CharMatrix.Position position) {
             this.unitType = unitType;
+            this.attackPower = attackPower;
             this.position = position;
         }
 
@@ -128,5 +154,17 @@ public class Day15 {
     enum UnitType {
         ELF,
         GOBLIN
+    }
+
+    static class Result {
+        final int rounds;
+        final int hitPointsLeft;
+        final boolean elfDied;
+
+        public Result(int rounds, int hitPointsLeft, boolean elfDied) {
+            this.rounds = rounds;
+            this.hitPointsLeft = hitPointsLeft;
+            this.elfDied = elfDied;
+        }
     }
 }
